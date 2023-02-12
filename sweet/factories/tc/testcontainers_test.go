8feca3316e4db1efa24431b241389d850ee1fa65
Barry Hennessy <barry@barryhennessy.com>
2023-02-12 22:35:50 +0100

    Add postgres test container

--- sweet/factories/tc/testcontainers_test.go
+++ sweet/factories/tc/testcontainers_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/barry-hennessy/test/sweet/factories/tc"
 	"github.com/barry-hennessy/test/sweet/factories/tc/cockroachdb"
 	"github.com/barry-hennessy/test/sweet/factories/tc/mongodb"
+	"github.com/barry-hennessy/test/sweet/factories/tc/postgres"
 	"github.com/barry-hennessy/test/sweet/factories/tc/redis"
 	"github.com/testcontainers/testcontainers-go"
 )
@@ -16,10 +17,15 @@ func TestNewFactory(t *testing.T) {
 	ctx := context.Background()
 	t.Run("containers start", func(t *testing.T) {
 		factories := map[string]tc.Container{
-			"redis:6":   redis.NewRedisContainer("redis:6"),
-			"redis:7":   redis.NewRedisContainer("redis:6"),
-			"crdb:21.1": cockroachdb.NewCockroachDBContainer("cockroachdb/cockroach:latest-v21.1"),
-			"mongo:6":   mongodb.NewMongoDBContainer("mongo:6"),
+			"redis:6":     redis.NewRedisContainer("redis:6"),
+			"redis:7":     redis.NewRedisContainer("redis:6"),
+			"crdb:21.1":   cockroachdb.NewCockroachDBContainer("cockroachdb/cockroach:latest-v21.1"),
+			"mongo:6":     mongodb.NewMongoDBContainer("mongo:6"),
+			"postgres:11": postgres.NewPostgresContainer("postgres:11"),
+			"postgres:12": postgres.NewPostgresContainer("postgres:12"),
+			"postgres:13": postgres.NewPostgresContainer("postgres:13"),
+			"postgres:14": postgres.NewPostgresContainer("postgres:14"),
+			"postgres:15": postgres.NewPostgresContainer("postgres:15"),
 		}
 
 		for name, container := range factories {
