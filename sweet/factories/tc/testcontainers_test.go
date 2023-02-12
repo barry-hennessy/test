4ebe210c4b169b4e8fe92ee82e0d1fa050a5d02e
Barry Hennessy <barry@barryhennessy.com>
2023-02-12 20:07:55 +0100

    Add cockroachdb testcontainer

--- sweet/factories/tc/testcontainers_test.go
+++ sweet/factories/tc/testcontainers_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/barry-hennessy/test/sweet"
 	"github.com/barry-hennessy/test/sweet/factories/tc"
+	"github.com/barry-hennessy/test/sweet/factories/tc/cockroachdb"
 	"github.com/barry-hennessy/test/sweet/factories/tc/redis"
 	"github.com/testcontainers/testcontainers-go"
 )
@@ -14,8 +15,9 @@ func TestNewFactory(t *testing.T) {
 	ctx := context.Background()
 	t.Run("containers start", func(t *testing.T) {
 		factories := map[string]tc.Container{
-			"redis:6": redis.NewRedisContainer("redis:6"),
-			"redis:7": redis.NewRedisContainer("redis:6"),
+			"redis:6":    redis.NewRedisContainer("redis:6"),
+			"redis:7":    redis.NewRedisContainer("redis:6"),
+			"crdb: 21.1": cockroachdb.NewCockroachDBContainer("cockroachdb/cockroach:latest-v21.1"),
 		}
 
 		for name, container := range factories {
